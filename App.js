0f8422567c8fa3211f9ea84f6abf6445ed475bba
darrny <167701231+darrny@users.noreply.github.com>
2024-06-07 00:17:31 +0800

    nil

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
+import { KeyboardAvoidingView } from 'react-native';
+import { TextInput } from 'react-native';
+import { Platform } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { Keyboard } from 'react-native';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Task</Text>
+        <View style={styles.items}>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key = {index} onPress = {() => completeTask(index)}>
+                  <Task text={item}/>
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+
+      <KeyboardAvoidingView
+        behaviour = {Platform.OS === 'ios' ? "paddinng" : "height"}
+        style = {styles.writeTaskWrapper}
+      >
+        <TextInput style = {styles.input} placeholder = {'Write a task'} value = {task} onChangeText={text => setTask(text)}/>
+
+        <TouchableOpacity onPress = {() => handleAddTask()}>
+          <View style = {styles.addWrapper}>
+            <Text style = {styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +59,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginBottom: 20,
+  },
+  items: {
+    justifyContent:'space-between',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    width: 250,
+    backgroundColor: 'white',
+    borderRadius: 60,
+    borderWidth: 1,
+    borderColor: 'grey',
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'white',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'grey',
+    borderWidth: 1,
+  },
+  addText: {
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    flexDirection: 'row',
+    bottom: 60,
+    width: '100%',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  }
 });
---
+++ components/Task.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+
+const Task = (props) => {
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <Text style={styles.itemText}>{props.text}</Text>
+            </View>
+        </View>
+    )
+}
+
+export default Task
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#FFF',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'centre',
+        marginBottom: 10,
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    },
+    square: {
+        width: 24,
+        height: 24,
+        borderRadius: 2,
+        borderWidth: 2,
+        borderColor: '#E8EAED',
+    },
+    itemText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#000000',
+        marginLeft: 10,
+        maxWidth: '80%',
+    },
+})
